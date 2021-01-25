3155cb2a1fd1b88141f40c4b408723ad44c6b6bb
Ana Luiza <analuizaramalho9@gmail.com>
2021-01-25 14:04:24 -0300

    inicial commit with splash screen

---
+++ desafio04/app/src/main/java/com/analuramalho/desafio04/splash/view/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.analuramalho.desafio04.splash.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.analuramalho.desafio04.MainActivity
+import com.analuramalho.desafio04.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+
+    }
+}
